7ae47c2b09aa5d9c57e18997ea3a30af31be181e
Xin Wang <mr.xin.wang@gmail..com>
2024-05-27 23:53:49 +0000

    update debug cmd

--- wd/conf/eksctl/yaml/update.sh
+++ wd/conf/eksctl/yaml/update.sh
@@ -7,4 +7,20 @@ kubectl get pods -l app=job-watcher -n kube-system | grep -oE '^[^ ]+'
 kubectl describe deployment job-watcher -n kube-system
 # kubectl logs -f job-watcher-6c5494df57-vgz9d --namespace=kube-system
 kubectl logs -f $(kubectl get pods -l app=job-watcher -n kube-system -o jsonpath='{.items[0].metadata.name}') -n kube-system
+
+# Test job creation
 kubectl create -f conf/eksctl/yaml/pytorchjob_mnist.yaml
+
+# Test node label change
+kubectl get node
+kubectl label nodes ip-192-168-141-155.us-west-2.compute.internal node-health-status=UnschedulablePendingReboot
+kubectl label nodes ip-192-168-141-155.us-west-2.compute.internal node-health-status=UnschedulablePendingReplacement
+kubectl label nodes ip-192-168-141-155.us-west-2.compute.internal node-health-status=SchedulablePreferred
+kubectl label nodes ip-192-168-141-155.us-west-2.compute.internal node-health-status=Schedulable
+kubectl get nodes ip-192-168-141-155.us-west-2.compute.internal --show-labels | grep Unschedulable
+kubectl label nodes ip-192-168-141-155.us-west-2.compute.internal node-health-status- --overwrite
+kubectl uncordon ip-192-168-141-155.us-west-2.compute.internal
+
+# Delete pod
+kubectl get pods -n kubeflow
+kubectl delete pod $(kubectl get pods -l app=job-watcher -n kube-system -o jsonpath='{.items[0].metadata.name}')
