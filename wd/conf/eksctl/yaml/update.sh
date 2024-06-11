75e55478bc094a0048cfe5916fa40eb894965ee3
Ubuntu <ubuntu@ip-172-31-7-24.us-west-2.compute.internal>
2024-06-11 21:29:29 +0000

    correct test job deployment config

--- wd/conf/eksctl/yaml/update.sh
+++ wd/conf/eksctl/yaml/update.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
+# install KTO
+# https://github.com/kubeflow/training-operator
+kubectl apply -k "github.com/kubeflow/training-operator/manifests/overlays/standalone?ref=v1.7.0"
+
+# create service role
+kubectl create -f conf/eksctl/yaml/test_rbac.yaml
+# deploy job watcher
+kubectl create -f conf/eksctl/yaml/test_job_watcher.yaml
+# in case deployment failed / debug deployment failure
+kubectl describe deployment job-watcher -n kube-system
+kubectl get pods -n kube-system --field-selector=status.phase!=Running -n kube-system
+kubectl describe replicaset job-watcher-77c69c857c -n kube-system
+
+# clean up and retry
 kubectl delete pytorchjob pt-test-job1 -n kubeflow
 kubectl delete deployment job-watcher -n kube-system
-kubectl create -f conf/eksctl/yaml/test_job_watcher.yaml
+
 kubectl get pods -l app=job-watcher -n kube-system
 kubectl get pods -l app=job-watcher -n kube-system | grep -oE '^[^ ]+'
-kubectl describe deployment job-watcher -n kube-system
+
 # kubectl logs -f job-watcher-6c5494df57-vgz9d --namespace=kube-system
 kubectl logs -f $(kubectl get pods -l app=job-watcher -n kube-system -o jsonpath='{.items[0].metadata.name}') -n kube-system
 
 # Test job creation
 kubectl create -f conf/eksctl/yaml/pytorchjob_mnist.yaml
+operator_pod_name=`kubectl get pods -n kubeflow | grep "training-operator" | awk -F' ' '{print $1}'`
+kubectl logs -n kubeflow $operator_pod_name
+
+# Get yaml file for the job
+kubectl get -o yaml pytorchjobs pt-test-job1 -n kubeflow
+
+# delete test job
+kubectl delete pytorchjob pt-test-job1 -n kubeflow
 
 # Test node label change
 kubectl get node
+
 kubectl label nodes ip-192-168-141-155.us-west-2.compute.internal node-health-status=UnschedulablePendingReboot
 kubectl label nodes ip-192-168-141-155.us-west-2.compute.internal node-health-status=UnschedulablePendingReplacement
 kubectl label nodes ip-192-168-141-155.us-west-2.compute.internal node-health-status=SchedulablePreferred
