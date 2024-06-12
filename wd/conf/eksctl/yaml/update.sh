b41e5c0836d5dda3f9bcedac0e9809a3ea4b1359
Ubuntu <ubuntu@ip-172-31-7-24.us-west-2.compute.internal>
2024-06-12 22:42:37 +0000

    Add IAM mapping role

--- wd/conf/eksctl/yaml/update.sh
+++ wd/conf/eksctl/yaml/update.sh
@@ -5,12 +5,27 @@ kubectl apply -k "github.com/kubeflow/training-operator/manifests/overlays/stand
 
 # create service role
 kubectl create -f conf/eksctl/yaml/test_rbac.yaml
+kubectl delete serviceaccount job-watcher -n kube-system
+kubectl delete clusterrolebinding job-watcher-binding
+kubectl delete clusterrole system:job-watcher
+
+kubectl create -f conf/eksctl/yaml/test_rbac_iam.yaml
+
 # deploy job watcher
 kubectl create -f conf/eksctl/yaml/test_job_watcher.yaml
 # in case deployment failed / debug deployment failure
 kubectl describe deployment job-watcher -n kube-system
 kubectl get pods -n kube-system --field-selector=status.phase!=Running -n kube-system
 kubectl describe replicaset job-watcher-77c69c857c -n kube-system
+
+# Test job creation
+kubectl create -f conf/eksctl/yaml/pytorchjob_mnist.yaml
+kubectl get pods -n kubeflow
+operator_pod_name=`kubectl get pods -n kubeflow | grep "training-operator" | awk -F' ' '{print $1}'`
+# only for 1 time debug
+kubectl logs -n kubeflow $operator_pod_name
+kubectl logs -f -n kubeflow $operator_pod_name
+
 
 # clean up and retry
 kubectl delete pytorchjob pt-test-job1 -n kubeflow
@@ -19,13 +34,9 @@ kubectl delete deployment job-watcher -n kube-system
 kubectl get pods -l app=job-watcher -n kube-system
 kubectl get pods -l app=job-watcher -n kube-system | grep -oE '^[^ ]+'
 
+# monitor job watcher logs
 # kubectl logs -f job-watcher-6c5494df57-vgz9d --namespace=kube-system
 kubectl logs -f $(kubectl get pods -l app=job-watcher -n kube-system -o jsonpath='{.items[0].metadata.name}') -n kube-system
-
-# Test job creation
-kubectl create -f conf/eksctl/yaml/pytorchjob_mnist.yaml
-operator_pod_name=`kubectl get pods -n kubeflow | grep "training-operator" | awk -F' ' '{print $1}'`
-kubectl logs -n kubeflow $operator_pod_name
 
 # Get yaml file for the job
 kubectl get -o yaml pytorchjobs pt-test-job1 -n kubeflow
@@ -36,13 +47,15 @@ kubectl delete pytorchjob pt-test-job1 -n kubeflow
 # Test node label change
 kubectl get node
 
-kubectl label nodes ip-192-168-141-155.us-west-2.compute.internal node-health-status=UnschedulablePendingReboot
-kubectl label nodes ip-192-168-141-155.us-west-2.compute.internal node-health-status=UnschedulablePendingReplacement
-kubectl label nodes ip-192-168-141-155.us-west-2.compute.internal node-health-status=SchedulablePreferred
-kubectl label nodes ip-192-168-141-155.us-west-2.compute.internal node-health-status=Schedulable
-kubectl get nodes ip-192-168-141-155.us-west-2.compute.internal --show-labels | grep Unschedulable
-kubectl label nodes ip-192-168-141-155.us-west-2.compute.internal node-health-status- --overwrite
-kubectl uncordon ip-192-168-141-155.us-west-2.compute.internal
+kubectl label nodes ip-192-168-129-59.us-west-2.compute.internal sagemaker.amazonaws.com/node-health-status=UnschedulablePendingReboot
+kubectl label nodes ip-192-168-129-59.us-west-2.compute.internal sagemaker.amazonaws.com/node-health-status- --overwrite
+
+kubectl label nodes ip-192-168-129-59.us-west-2.compute.internal sagemaker.amazonaws.com/node-health-status=UnschedulablePendingReplacement
+kubectl label nodes ip-192-168-129-59.us-west-2.compute.internal sagemaker.amazonaws.com/node-health-status=SchedulablePreferred
+kubectl label nodes ip-192-168-129-59.us-west-2.compute.internal sagemaker.amazonaws.com/node-health-status=Schedulable
+kubectl get nodes ip-192-168-129-59.us-west-2.compute.internal --show-labels | grep Unschedulable
+kubectl label nodes ip-192-168-129-59.us-west-2.compute.internal sagemaker.amazonaws.com/node-health-status- --overwrite
+kubectl uncordon ip-192-168-129-59.us-west-2.compute.internal
 
 # Delete pod
 kubectl get pods -n kubeflow
