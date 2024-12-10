efd290b5d1c6ab30dd6ee86d29c3885d4737aa2c
Ubuntu <ubuntu@ip-172-31-42-172.us-west-2.compute.internal>
2024-12-10 06:13:53 +0000

    update 12-09-2024

--- wd/conf/eksctl/yaml/update.sh
+++ wd/conf/eksctl/yaml/update.sh
@@ -81,9 +81,9 @@ kubectl get node
 
 kubectl describe pod -n kubeflow pt-job-1-worker-1
 
-kubectl label nodes ip-192-168-239-97.us-west-2.compute.internal sagemaker.amazonaws.com/node-health-status=UnschedulablePendingReplacement
+kubectl label nodes hyperpod-i-077d281abaef65046 sagemaker.amazonaws.com/node-health-status=UnschedulablePendingReplacement
 kubectl label nodes ip-192-168-164-40.us-west-2.compute.internal sagemaker.amazonaws.com/node-health-status=UnschedulablePendingReboot
-kubectl label nodes ip-192-168-164-40.us-west-2.compute.internal sagemaker.amazonaws.com/node-health-status- --overwrite
+kubectl label nodes hyperpod-i-077d281abaef65046 sagemaker.amazonaws.com/node-health-status- --overwrite
 
 # "ip-192-168-154-218.us-west-2.compute.internal"
 # force delete pod
@@ -95,9 +95,12 @@ pod "pt-job-1-worker-0" force deleted
 kubectl describe pod -n kubeflow pt-job-1-worker-1
 kubectl label nodes i-0194083c3e0e352e7.us-west-2.compute.internal sagemaker.amazonaws.com/node-health-status=SchedulablePreferred
 kubectl label nodes ip-192-168-212-8.us-west-2.compute.internal sagemaker.amazonaws.com/node-health-status=Schedulable
-kubectl get nodes ip-192-168-129-59.us-west-2.compute.internal --show-labels | grep Unschedulable
+kubectl get nodes hyperpod-i-0f38d8a021f279074 --show-labels | grep Unschedulable
 kubectl label nodes ip-192-168-252-176.us-west-2.compute.internal sagemaker.amazonaws.com/node-health-status- --overwrite
 kubectl uncordon ip-192-168-129-59.us-west-2.compute.internal
+
+# check node labels
+kubectl get nodes hyperpod-i-032366fbbe49905d3 --show-labels | grep sagemaker.amazonaws.com
 
 # Delete pod
 kubectl get pods -n kubeflow
@@ -133,13 +136,35 @@ docker tag 321098965911.dkr.ecr.us-east-2.amazonaws.com/probe:megatron-llm 65465
 docker push 654654592687.dkr.ecr.us-west-2.amazonaws.com/probe:megatron-llm
 
 # scale tes
-kubectl apply -f ./etcv2.yaml
+kubectl apply -f ./scale-etcv2.yaml
+kubectl get services -n kubeflow
+kubectl describe service etcd -n kubeflow
+kubectl get pods -l app=etcd -n kubeflow
 sleep 5
-kubectl apply -f ./megatron-lm.yaml
+kubectl apply -f ./scale-megatron-lm.yaml
+kubectl get pods -n kubeflow | grep Running | wc -l
+kubectl get pods -n kubeflow | grep megatron-llm | grep Running | wc -l
 
-kubectl delete -f ./megatron-lm.yaml
-kubectl delete -f ./etcv2.yaml
+kubectl describe pod -n kubeflow megatron-llm-worker-997
 
+
+
+kubectl delete -f ./scale-megatron-lm.yaml
+kubectl delete -f ./scale-etcv2.yaml
+
+kubectl edit daemonset -n kube-system aws-node -o yaml
+## then add
+- name: MAX_ENI
+  value: "1"
+- name: MINIMUM_IP_TARGET
+  value: "1"
+- name: WARM_IP_TARGET
+  value: "1"
+- name: WARM_ENI_TARGET
+  value: "0"
+- name: WARM_PREFIX_TARGET
+  value: "1"
+# need to delete existing value for WARM_ENI_TARGET and WARM_PREFIX_TARGET
 
 ## Jump host
 sudo apt install unzip
@@ -147,7 +172,8 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 
-aws eks update-kubeconfig --region us-west-2 --name hyperpod-scaling-benchmark-eks-cluster-g5
+aws eks update-kubeconfig --region us-west-2 --name agi-hyperpod-eks-cluster
+
 
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.3/2024-04-19/bin/linux/amd64/kubectl
 chmod +x ./kubectl
@@ -158,7 +184,7 @@ sudo cp ./kubectl /usr/sbin
 kubectl delete pytorchjob pytorch-simple -n kubeflow
 
 # create Hyperpod cluster
-cd /workplace/xwnamz/HyperPodEKSTests/bin
+cd HyperPodEKSTests/bin
 
 SUBNET=$(cfn-output $VPC_STACK_NAME PrivateSubnet1)
 SECURITY_GROUP=$(cfn-output $VPC_STACK_NAME NoIngressSecurityGroup)
@@ -217,3 +243,121 @@ aws sagemaker --endpoint $ENDPOINT describe-cluster --cluster-name arn:aws:sagem
 aws sagemaker --endpoint $ENDPOINT delete-cluster --cluster-name arn:aws:sagemaker:us-west-2:891377004071:cluster/jl12jfn14b1a
 aws sagemaker-dev --endpoint $ENDPOINT delete-cluster --cluster-name arn:aws:sagemaker:us-west-2:891377004071:cluster/jl12jfn14b1a
 aws sagemaker delete-cluster --cluster-name hyperpod-eks-test-1726982497
+# AGI
+kubectl get pods -n obsidian
+kubectl describe pod -n obsidian lev-470b-hyperpod-1512-3g5e-worker-1378
+kubectl get nodes hyperpod-i-03571c4d67c2903eb --show-labels
+kubectl get pods --field-selector=spec.nodeName=hyperpod-i-03571c4d67c2903eb --all-namespaces
+
+kubectl get pods --field-selector=spec.nodeName=hyperpod-i-03571c4d67c2903eb --all-namespaces
+NAMESPACE                   NAME                                                       READY   STATUS    RESTARTS      AGE
+amazon-cloudwatch           cloudwatch-agent-grphf                                     1/1     Running   1 (13m ago)   25h
+amazon-cloudwatch           dcgm-exporter-m7pmk                                        1/1     Running   1 (13m ago)   25h
+amazon-cloudwatch           fluent-bit-pvkq2                                           1/1     Running   2 (12m ago)   25h
+amazon-guardduty            aws-guardduty-agent-dpg84                                  1/1     Running   1 (13m ago)   25h
+aws-efa-k8s-device-plugin   aws-efa-k8s-device-plugin-tpqwx                            1/1     Running   1 (13m ago)   25h
+aws-for-fluent-bit          aws-for-fluent-bit-w6tk4                                   1/1     Running   2 (12m ago)   23h
+aws-fsx-csi-driver          fsx-csi-node-wdn6k                                         3/3     Running   3 (13m ago)   25h
+aws-hyperpod                hyperpod-ml-health-monitoring-agent-s474t                  1/1     Running   1 (13m ago)   25h
+dcgm-exporter               dcgm-exporter-5jgq2                                        1/1     Running   0             10m
+eks-pod-identity-agent      eks-pod-identity-agent-8nzbj                               1/1     Running   1 (13m ago)   25h
+kube-system                 aws-node-fcpbt                                             2/2     Running   4 (11m ago)   25h
+kube-system                 kube-proxy-qhhn8                                           1/1     Running   1 (13m ago)   25h
+nvidia-device-plugin        nvidia-device-plugin-gpu-feature-discovery-zv7sb           1/1     Running   1 (13m ago)   25h
+nvidia-device-plugin        nvidia-device-plugin-node-feature-discovery-worker-5bztf   1/1     Running   5 (13m ago)   25h
+nvidia-device-plugin        nvidia-device-plugin-tsfbl                                 1/1     Running   1 (13m ago)   25h
+prometheus                  kube-prometheus-stack-prometheus-node-exporter-gfs2j       1/1     Running   1 (13m ago)   25h
+
+
+kubectl get pods -n obsidian | grep ^lev-470b-hyperpod-1512-3t38
+
+
+# scale test
+aws eks update-kubeconfig --region us-west-2 --name agi-hyperpod-eks-cluster
+aws eks update-kubeconfig --region us-west-2 --name eks-scale-test-g5
+kubectl config current-context
+
+python3 -m venv /home/ubuntu/hyperpod-cli-venv                                                                          │
+source /home/ubuntu/hyperpod-cli-venv/bin/activate
+
+helm dependencies update helm_chart/HyperPodHelmChart
+helm install dependencies helm_chart/HyperPodHelmChart --dry-run
+helm install dependencies helm_chart/HyperPodHelmChart --namespace kube-system
+
+# validate 
+kubectl get namespaces
+kubectl describe clusterrole job-auto-restart
+
+# Create personal stack
+# https://docs.aws.amazon.com/sagemaker/latest/dg/sagemaker-hyperpod-eks-operate-console-ui-create-cluster.html
+export VPC_STACK_NAME=agi-hyperpod-vpc-stack
+export SAGEMAKER_STACK_NAME=agi-hyperpod-sm-stack
+export EKS_STACK_NAME=agi-hyperpod-eks-stack
+
+# under HyperPodEKSTests/bin
+export SUBNET1=$(./cfn-output $VPC_STACK_NAME PrivateSubnet1) && echo $SUBNET1
+export SUBNET2=$(./cfn-output $VPC_STACK_NAME PrivateSubnet2) && echo $SUBNET2
+export SUBNET3=$(./cfn-output $VPC_STACK_NAME PrivateSubnet3) && echo $SUBNET3
+export SECURITY_GROUP=$(./cfn-output $VPC_STACK_NAME NoIngressSecurityGroup) && echo $SECURITY_GROUP
+
+export SUBNET=$(./cfn-output $VPC_STACK_NAME PrivateSubnet2) && echo $SUBNET
+export SECURITY_GROUP=$(./cfn-output $VPC_STACK_NAME NoIngressSecurityGroup) && echo $SECURITY_GROUP
+export EKS_CLUSTER_ARN=$(./cfn-output $EKS_STACK_NAME ClusterArn) && echo $EKS_CLUSTER_ARN
+export EXECUTION_ROLE=$(./cfn-output $SAGEMAKER_STACK_NAME ExecutionRole) && echo $EXECUTION_ROLE
+export SERVICE_ROLE=$(./cfn-output $SAGEMAKER_STACK_NAME ServiceRole) && echo $SERVICE_ROLE
+export BUCKET_NAME=$(./cfn-output $SAGEMAKER_STACK_NAME Bucket) && echo $BUCKET_NAME
+export HP_CLUSTER_NAME="hyperpod-sacle-test-g5" && echo $HP_CLUSTER_NAME
+
+aws eks update-kubeconfig --name $(cfn-output $EKS_STACK_NAME ClusterName)
+kubectl create namespace aws-hyperpod
+
+aws s3 cp lifecyclescripts/on_create_noop.sh s3://$BUCKET_NAME
+
+aws sagemaker create-cluster --region us-west-2 \
+    --cluster-name $HP_CLUSTER_NAME \
+    --orchestrator '{"Eks": {"ClusterArn":"'$EKS_CLUSTER_ARN'"}}' \
+    --instance-groups '{
+        "InstanceGroupName": "compute-nodes-g5",
+        "InstanceType": "ml.g5.8xlarge",
+        "InstanceCount": 8,
+        "LifeCycleConfig": {
+            "SourceS3Uri": "s3://'$BUCKET_NAME'",
+            "OnCreate": "on_create_noop.sh"
+        },
+        "ExecutionRole": "'$EXECUTION_ROLE'",
+        "ThreadsPerCore": 1
+    }' \
+    --instance-groups '{
+        "InstanceGroupName": "compute-nodes-t3",
+        "InstanceType": "ml.t3.medium",
+        "InstanceCount": 8,
+        "LifeCycleConfig": {
+            "SourceS3Uri": "s3://'$BUCKET_NAME'",
+            "OnCreate": "on_create_noop.sh"
+        },
+        "ExecutionRole": "'$EXECUTION_ROLE'",
+        "ThreadsPerCore": 1
+    }' \
+    --vpc-config '{
+        "SecurityGroupIds": ["'$SECURITY_GROUP'"],
+        "Subnets": ["'$SUBNET'"]
+    }'
+
+aws sagemaker update-cluster --region us-west-2 \
+    --cluster-name $HP_CLUSTER_NAME \
+    --instance-groups '{
+        "InstanceGroupName": "compute-nodes",
+        "InstanceType": "ml.g5.8xlarge",
+        "InstanceCount": 1530,
+        "LifeCycleConfig": {
+            "SourceS3Uri": "s3://'$BUCKET_NAME'",
+            "OnCreate": "on_create_noop.sh"
+        },
+        "ExecutionRole": "'$EXECUTION_ROLE'",
+        "ThreadsPerCore": 1
+    }'
+
+aws sagemaker delete-cluster --region us-west-2 \
+    --cluster-name $HP_CLUSTER_NAME
+
+arn:aws:sagemaker:us-west-2:654654592687:cluster/qovn86muc8d6
